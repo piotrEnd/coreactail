046636ee62b576aad1fbf5c24eeb1722c2ddb192
piotrEnd <piotrend@gmail.com>
2019-04-08 23:27:28 +0200

    Updates

---
+++ precache-manifest.d5e00cfce1b9bf74122852e28c586331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a663820356ef54ef97697335ae141d31",
+    "url": "/coreactail/static/media/bg_default.a6638203.jpg"
+  },
+  {
+    "revision": "bb3ebbe7bb94a8a6d7f8",
+    "url": "/coreactail/static/js/runtime~main.e5301717.js"
+  },
+  {
+    "revision": "2f234fc0d27154d05b66",
+    "url": "/coreactail/static/js/main.27ca041b.chunk.js"
+  },
+  {
+    "revision": "2aba7b94bfc474e7c636",
+    "url": "/coreactail/static/js/2.108e142a.chunk.js"
+  },
+  {
+    "revision": "2f234fc0d27154d05b66",
+    "url": "/coreactail/static/css/main.6d7723f6.chunk.css"
+  },
+  {
+    "revision": "2aba7b94bfc474e7c636",
+    "url": "/coreactail/static/css/2.c7733192.chunk.css"
+  },
+  {
+    "revision": "9e80d7ff6a52f528655085ff11b6db78",
+    "url": "/coreactail/index.html"
+  }
+];
