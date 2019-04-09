78c53046421320329f7a62d97bdf03eafeb1cb08
piotrEnd <piotrend@gmail.com>
2019-04-09 12:16:25 +0200

    replacing fontawesome-cdn with react-icons

--- src/components/DrinkSearch.js
+++ src/components/DrinkSearch.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
+import { IoIosSearch } from 'react-icons/io';
 
 export default class DrinkSearch extends Component {
 	render() {
 		const { value, handleChange, handleSubmit, placeholder } = this.props;
 
 		return (
-				<div className="container">
-					<div className="row">
-						<div className="col-10 mx-auto col-md-4 mt-2 text-center">
-							<form className="mb-5 outline" onSubmit={handleSubmit}>
-								<div className="input-group">
-									<input
-										type="text"
-										name="search"
-										className="form-control"
-										placeholder={placeholder}
-										value={value}
-										onChange={handleChange}
-									/>
-									<div className="input-group-append">
-										<button
-											type="submit"
-											className="input-group-text "
-											variant="outline-dark"
-										>
-											<i className="fas fa-search" />
-										</button>
-									</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-10 mx-auto col-md-4 mt-2 text-center">
+						<form className="mb-5 outline" onSubmit={handleSubmit}>
+							<div className="input-group">
+								<input
+									type="text"
+									name="search"
+									className="form-control"
+									placeholder={placeholder}
+									value={value}
+									onChange={handleChange}
+								/>
+								<div className="input-group-append">
+									<button
+										type="submit"
+										className="input-group-text "
+										variant="outline-dark"
+									>
+										<IoIosSearch />
+									</button>
 								</div>
-							</form>
-						</div>
+							</div>
+						</form>
 					</div>
 				</div>
+			</div>
 		);
 	}
 }
